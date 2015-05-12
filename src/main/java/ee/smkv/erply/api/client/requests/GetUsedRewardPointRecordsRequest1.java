34138abb6710b3b667187b577a06a38fe71fcb1c
smkv <!Natasa1>
2015-05-12 21:30:51 +0300

    editUsedRewardPointRecord

---
+++ src/main/java/ee/smkv/erply/api/client/requests/GetUsedRewardPointRecordsRequest1.java
@@ -0,0 +1,8 @@
+package ee.smkv.erply.api.client.requests;
+
+public class GetUsedRewardPointRecordsRequest1 extends GetEarnedRewardPointRecordsRequest {
+
+    public GetUsedRewardPointRecordsRequest1() {
+        super("getUsedRewardPointRecords");
+    }
+}
--- src/main/java/ee/smkv/erply/api/client/requests/getUsedRewardPointRecordsRequest.java
+++
@@ -1,8 +0,0 @@
-package ee.smkv.erply.api.client.requests;
-
-public class getUsedRewardPointRecordsRequest extends GetEarnedRewardPointRecordsRequest {
-
-    public getUsedRewardPointRecordsRequest() {
-        super("getUsedRewardPointRecords");
-    }
-}
